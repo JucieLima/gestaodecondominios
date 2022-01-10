69fe1ed55345dae5baf76c656eae6e12dc5512a0
JucieLima <jucielima@gmail.com>
2022-01-10 10:56:30 -0300

    Documentos e boletos

--- app/Http/Controllers/BilletController.php
+++ app/Http/Controllers/BilletController.php
@@ -2,9 +2,32 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Billet;
+use App\Models\Unit;
 use Illuminate\Http\Request;
 
 class BilletController extends Controller
 {
-    //
+    public function getAll(Request $request)
+    {
+        $array = ['error' => ''];
+        $property = $request->input('property');
+        if($property){
+            $user = auth()->user()['id'];
+            $unit = Unit::where('id', $property)->where('owner', $user)->count();
+            if(!$unit){
+                $array['error'] = 'A unidade requisitada não pertence a este usuário!';
+                $array['user'] = $user;
+                return $array;
+            }
+            $billets = Billet::where('unit', $property)->get();
+            foreach($billets as $key => $value){
+                $billets[$key]['file'] = asset('storage/'.$value['file_url']);
+            }
+            $array['list'] = $billets;
+        }else{
+            $array['error'] = 'Para gerar boletos é necessário informar uma unidade!';
+        }
+        return $array;
+    }
 }
--- app/Http/Controllers/DocumentController.php
+++ app/Http/Controllers/DocumentController.php
@@ -2,9 +2,21 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Document;
 use Illuminate\Http\Request;
 
 class DocumentController extends Controller
 {
-    //
+    public function getAll()
+    {
+        $array = ['error' => ''];
+        $docs = Document::all();
+        foreach ($docs as $key => $value){
+            $docs[$key]['file'] = asset('storage/'.$value['file_url']);
+        }
+
+        $array['list'] = $docs;
+
+        return $array;
+    }
 }
